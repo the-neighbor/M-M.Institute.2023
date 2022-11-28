0cbe7f412fa9aa8ebe1a76db63f82ee47d4bf87f
Olive Daly <olive-branch@posteo.me>
2022-11-28 12:39:04 -0600

    set up app structure

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+
+function Home() {
+  return (
+    <div className="Home">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
